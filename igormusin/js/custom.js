de125500bdf16847b8ca6d34cd8143586899af63
Alexander Van Maele <alexander@atlesque.com>
2019-01-10 11:25:31 +0100

    Cleanup redundant files and add lightbox gallery

--- igormusin/js/custom.js
+++ igormusin/js/custom.js
@@ -1,16 +1,21 @@
 /* global jQuery, ImageViewer */
 (function ($) {
   $(document).ready(() => {
+    // Init ImageViewer on attachment page
     var viewer = ImageViewer()
     $('.entry-attachment img').click((e) => {
       var imgSrc = $(e.currentTarget).parent().attr('data-attachment-link')
       viewer.show(imgSrc)
     })
 
+    // Bind ESC key to close ImageViewer modal
     $(document).keyup((e) => {
       if (e.keyCode === 27) { // ESC key
         viewer.hide()
       }
     })
+
+    // Init fancybox image gallery on gallery page
+    $('.gallery-item a').fancybox({ hideScrollbar: false }).attr('data-fancybox', 'gallery');
   })
 })(jQuery)
